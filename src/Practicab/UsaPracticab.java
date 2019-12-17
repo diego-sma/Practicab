5f16cb3bc0113f6fd3aed7cbb1d248cbc70b1ac8
USUARIO <USUARIO@LAPTOP-5VO18K48>
2019-12-17 07:53:04 -0500

    Proyecto para Practica

---
+++ src/Practicab/UsaPracticab.java
@@ -0,0 +1,15 @@
+
+package Practicab;
+
+
+public class UsaPracticab {
+    public static void main(String[] args) {
+        
+        Practica p=new Practica();
+        p.setVisible(true);
+       
+        p.setLocationRelativeTo(null);
+        
+        
+    }
+}
